dd32164cae280cfb7f2e881ffbbe3ea7f83bf29d
UniPlanner <uni@planner.local>
2026-07-30 09:16:05 +0800

    fix: use http scheme with cleartext + trailing slash URL

--- android/app/src/main/java/com/uniplanner/app/MainActivity.java
+++ android/app/src/main/java/com/uniplanner/app/MainActivity.java
@@ -1,5 +1,16 @@
-package com.uniplanner.app;
+﻿package com.uniplanner.app;
 
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.webkit.WebViewAssetLoader;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
